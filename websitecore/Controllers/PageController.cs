96f544c021f8d98bdf6ec82562fa4755102bfb27
Khoa Tran Dang <khoatd@smartosc.com>
2017-10-04 18:15:26 +0700

    Add project files.

---
+++ websitecore/Controllers/PageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace websitecore.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Header()
+        {
+            return View();
+        }
+
+        public ActionResult TopHeader()
+        {
+            return View();
+        }
+
+        public ActionResult TopFooter()
+        {
+            return View();
+        }
+
+        public ActionResult BottomFooter()
+        {
+            return View();
+        }
+    }
+}
---
+++ websitecore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="websitecore.MvcApplication" Language="C#" %>
