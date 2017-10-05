e92d0e2a33b95336b0fef6132aa3a213663d19e2
Khoa Tran Dang <khoatd@smartosc.com>
2017-10-05 18:02:01 +0700

    GetData

--- websitecore/Controllers/PageController.cs
+++ websitecore/Controllers/PageController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using websitecore.Models;
+using websitecore.ViewModel;
 
 namespace websitecore.Controllers
 {
@@ -19,12 +23,53 @@ namespace websitecore.Controllers
             return View();
         }
 
+        public ActionResult Menu()
+        {
+            Sitecore.Data.Database context = Sitecore.Context.Database;
+            Sitecore.Data.Items.Item item = context.Items[new ID("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")];
+            var menu = item.Children["Menu"];
+            var menuViewModel= new MenuViewModel();
+            menuViewModel.lstMenu=new List<Menu>();
+            foreach (Item i in menu.Children)
+            {
+                var menuName = i.Fields["MenuTitle"].Value;
+                var menuLink = Sitecore.Context.Database.GetItem(ID).Fields["MenuTitle"].Value;
+                var obj = new Menu()
+                {
+                    MenuTitle = menuName,
+                    MenuLink = menuLink
+                };
+                menuViewModel.lstMenu.Add(obj);
+            }
+            return View(menuViewModel);
+        }
+
         public ActionResult TopFooter()
         {
             return View();
         }
 
         public ActionResult BottomFooter()
+        {
+            return View();
+        }
+
+        public ActionResult Col1TopFooter()
+        {
+            return View();
+        }
+
+        public ActionResult Col2TopFooter()
+        {
+            return View();
+        }
+
+        public ActionResult Col3TopFooter()
+        {
+            return View();
+        }
+
+        public ActionResult Col4TopFooter()
         {
             return View();
         }
---
+++ websitecore/ViewModel/MenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using websitecore.Models;
+
+namespace websitecore.ViewModel
+{
+    public class MenuViewModel
+    {
+        public List<Menu> lstMenu { get; set; }
+    }
+}
